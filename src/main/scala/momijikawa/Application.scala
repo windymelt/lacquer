1532c0f79750de9c4508da79b7868ef002fc903d
Momijikawa <windy.imlovinit@gmail.com>
2014-10-09 00:43:19 +0900

    画像をキャッシュできるようになった
    コミットし直し

--- src/main/scala/momijikawa/Application.scala
+++ src/main/scala/momijikawa/Application.scala
@@ -1,5 +1,24 @@
 package momijikawa
 
-class Application extends  {
+import akka.actor.{ Props, ActorSystem }
+import akka.io.IO
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import spray.can.Http
+import spray.can.Http.ClientConnectionType
 
+object Application extends App {
+  override def main(args: Array[String]) = {
+    implicit val timeout: Timeout = 5 seconds
+    implicit val system = ActorSystem("lacquer")
+    implicit val execContext = system.dispatcher
+
+    println("actorSystem, execContext initialized")
+
+    println("setup created")
+
+    val service = system.actorOf(Props[Lacquer])
+    IO(Http) ! Http.Bind(service, "0.0.0.0", port = 8082)
+  }
 }
--- src/main/scala/momijikawa/Lacquer.scala
+++ src/main/scala/momijikawa/Lacquer.scala
@@ -1,17 +1,70 @@
 package momijikawa
 
-import akka.actor.Actor.Receive
-import akka.actor.ActorRef
+import akka.actor.Actor
+import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
+import spray.caching.{ Cache, LruCache }
+import spray.can.Http
+import spray.httpx.marshalling.{ MetaToResponseMarshallers, ToResponseMarshaller }
+import spray.routing.{ HttpServiceActor, Route, RequestContext }
+import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 import spray.http.HttpResponse
-import spray.routing._
+import spray.caching
+import MetaToResponseMarshallers.futureMarshaller
+import scalaz._
+import Scalaz._
 
-class Lacquer(connector: ActorRef) extends HttpServiceActor {
-  implicit val timeout: Timeout = Timeout(5 seconds)
+class Lacquer extends HttpServiceActor {
+  self: Actor ⇒
+  implicit val actorSystem = this.context.system
+  implicit val timeout: Timeout = 5 seconds
 
-  val route: Route = (ctx: RequestContext) => ctx.complete(connector.ask(ctx.request).mapTo[HttpResponse])
+  val cacheEverything: Cache[HttpResponse] = LruCache()
+
+  val route: Route = (ctx: RequestContext) ⇒ {
+    println(ctx.request.uri)
+    cacheEverything.get(ctx.request.message) match {
+      case Some(cache: Future[HttpResponse]) ⇒
+        println("found cache")
+        ctx.complete(cache)
+
+      case None ⇒
+        println("no cache")
+        val response: Future[HttpResponse] = (IO(Http) ? ctx.request).mapTo[HttpResponse]
+        val noCacheFlag = Seq("private", "no-cache", "no-store", "must-revalidate")
+
+        def cacheable(resp: HttpResponse): Boolean = {
+          val isCacheProhibited = for (
+            cacheControl ← resp.headers.find(header ⇒ header.name == "Cache-Control")
+          ) yield noCacheFlag.exists(value ⇒ cacheControl.value.contains(value))
+
+          val hasProhibitPragma = for (
+            pragma ← resp.headers.find(header ⇒ header.name == "Pragma")
+          ) yield pragma.value.contains("no-cache")
+
+          !(isCacheProhibited | false || (hasProhibitPragma | false))
+        }
+
+        def isImage(resp: HttpResponse): Boolean = {
+          val responseHasImage = for (
+            contentType ← resp.headers.find(header ⇒ header.name == "Content-Type")
+          ) yield contentType.value.contains("image/")
+          responseHasImage | false
+        }
+
+        response.onSuccess {
+          case resp if cacheable(resp) && isImage(resp) ⇒
+            cacheEverything(ctx.request.message, () ⇒ Future(resp))
+        }
+
+        ctx.complete(response)
+      case others ⇒
+        println(s"unknown: $others")
+    }
+  }
 
   def receive: Receive = runRoute(route)
 }
