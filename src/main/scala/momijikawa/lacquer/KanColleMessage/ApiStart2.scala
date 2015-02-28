cdc3ad4ab90ddfa14ec96a3880bf70828a981b63
windymelt <windy.imlovinit@gmail.com>
2015-03-01 00:56:25 +0900

    リファクタリング

--- src/main/scala/momijikawa/lacquer/KanColleMessage/ApiStart2.scala
+++ src/main/scala/momijikawa/lacquer/KanColleMessage/ApiStart2.scala
@@ -1,6 +1,6 @@
 package momijikawa.lacquer.KanColleMessage
 
-import spray.http.{HttpRequest, HttpResponse}
+import spray.http.{ HttpRequest, HttpResponse }
 
 import scalaz._
 
@@ -10,12 +10,8 @@ object ApiStart2Converter extends KanColleMessageConverter {
   import org.json4s._
   implicit val formats = DefaultFormats
 
-  def apply(message: HttpRequest): \/[HttpResponse ⇒ KanColleMessage, HttpRequest] = {
-    """kcsapi\/api_start2""".r.findFirstIn(message.uri.path.toString()) match {
-      case Some(_) ⇒ -\/(response2Message)
-      case None    ⇒ \/-(message)
-    }
-  }
+  val uriRegex = """kcsapi\/api_start2"""
+
   def response2Message(response: HttpResponse): ApiStart2 = {
     ApiStart2(getJsonString(response))
   }
